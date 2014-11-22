4e591b853a61093a24e73ba700a11c349b3efa6d
Goclis Yao <goclisyyh@gmail.com>
2014-11-22 17:24:17 +0800

    Prepare to move enum type into MccPublicType.h

---
+++ mcc/mcc/MccPublicType.h
@@ -0,0 +1,17 @@
+/**
+ * @author Goclis Yao
+ * @email goclisyyh@gmail.com
+ */
+
+enum TYPE_SPEC {
+	VOID_TYPE_SPEC,
+	INT_TYPE_SPEC
+};
+
+enum BINARY_OPERATOR {
+
+};
+
+enum UNARY_OPERATOR {
+
+};
