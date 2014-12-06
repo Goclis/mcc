244457f12e2033a60fce1acf6d6991f995841d72
Goclis Yao <goclisyyh@gmail.com>
2014-12-06 23:18:07 +0800

    Add input and output

--- mcc/mcc/main.cpp
+++ mcc/mcc/main.cpp
@@ -1,14 +1,35 @@
 #include <stdio.h>
 #include <cstdio>
+#include <fstream>
 #include "MccRobot.h"
+using namespace std;
 extern int yyparse();
 extern FILE *yyin;
 
-int main()
+int main(int args, char** argv)
 {
-	yyin = fopen("test", "r");
+	MccRobot &robot = theMccRobot();
+	string output_filename = "test";
+	string input_filename = "test";
+	if (args > 1) {
+		input_filename = string(argv[1]);
+		int pos = input_filename.find('.');
+		if (pos != -1) {
+			output_filename = input_filename.substr(0, pos);
+		}
+	}
+
+	yyin = fopen(input_filename.c_str(), "r");
+
 	yyparse();
 
-	theMccRobot().generate_code();
+	robot.generate_code();
+	
+	if (input_filename == output_filename) {
+		output_filename += ".out";
+	}
+	ofstream out_stream(output_filename);
+	out_stream << robot.get_global_var_code_buffer() << robot.get_code_buffer();
+
 	return 0;
 }
