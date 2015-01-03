0b8a0ce20c6893453b946fb77f89aa2872b613ac
Goclis Yao <goclisyyh@gmail.com>
2015-01-03 23:54:59 +0800

    Optimize if statement generation

--- mcc/mcc/MccIfStatement.cpp
+++ mcc/mcc/MccIfStatement.cpp
@@ -44,6 +44,11 @@ int MccIfStatement::generate_code() const
 	if_label += robot.generate_branch_label();
 	string false_branch_label = if_label + "_false";
 	string if_end = if_label + "_end";
+	
+	// No else, false_label == end_label.
+	if (nullptr == m_else) {
+		false_branch_label = if_end;
+	}
 
 	// Gen(m_condition).
 	this->m_condition->generate_code();
@@ -56,19 +61,22 @@ int MccIfStatement::generate_code() const
 
 	// Gen(m_if).
 	this->m_if->generate_code();
-	code_buffer += 
-		"j " + if_end + "\n"
-		"srlv $v1, $zero, $zero\n";
-	robot
-		.add_code("j " + if_end)
-		.add_code("srlv $v1, $zero, $zero");
-
+	if (nullptr != m_else) {
+		// Have else, need to jump to if_end at the end of if block.
+		code_buffer +=
+			"j " + if_end + "\n"
+			"srlv $v1, $zero, $zero\n";
+		robot
+			.add_code("j " + if_end)
+			.add_code("srlv $v1, $zero, $zero");
+	}
+	
 	// Gen(m_else), if have.
-	code_buffer += 
-		false_branch_label + ":\n";
-	robot
-		.add_code(false_branch_label + ":");
 	if (this->m_else != nullptr) {
+		code_buffer +=
+			false_branch_label + ":\n";
+		robot
+			.add_code(false_branch_label + ":");
 		this->m_else->generate_code();
 	}
 
